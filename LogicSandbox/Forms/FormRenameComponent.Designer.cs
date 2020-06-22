eaff7848b741a9d336f9eb01959ab9b26ac1e0c1
Maxstupo <maxstupo@hotmail.com>
2020-06-22 21:03:53 +1000

    Add component renaming with double-click.

---
+++ LogicSandbox/Forms/FormRenameComponent.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Maxstupo.LogicSandbox.Forms {
+    partial class FormRenameComponent {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnOkay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtComponentLabel = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOkay
+            // 
+            this.btnOkay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOkay.Location = new System.Drawing.Point(228, 44);
+            this.btnOkay.Name = "btnOkay";
+            this.btnOkay.Size = new System.Drawing.Size(75, 23);
+            this.btnOkay.TabIndex = 2;
+            this.btnOkay.Text = "&Okay";
+            this.btnOkay.UseVisualStyleBackColor = true;
+            this.btnOkay.Click += new System.EventHandler(this.btnOkay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Label:";
+            // 
+            // txtComponentLabel
+            // 
+            this.txtComponentLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtComponentLabel.Location = new System.Drawing.Point(55, 12);
+            this.txtComponentLabel.Name = "txtComponentLabel";
+            this.txtComponentLabel.Size = new System.Drawing.Size(329, 22);
+            this.txtComponentLabel.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(309, 44);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FormRenameComponent
+            // 
+            this.AcceptButton = this.btnOkay;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(396, 79);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtComponentLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOkay);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximumSize = new System.Drawing.Size(720, 118);
+            this.MinimumSize = new System.Drawing.Size(197, 118);
+            this.Name = "FormRenameComponent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Rename Component...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOkay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtComponentLabel;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ LogicSandbox/Forms/FormRenameComponent.cs
@@ -0,0 +1,33 @@
+﻿namespace Maxstupo.LogicSandbox.Forms {
+
+    using System;
+    using System.Windows.Forms;
+    using Maxstupo.LogicSandbox.Logic.Components;
+
+    public partial class FormRenameComponent : Form {
+
+        private readonly DigitalComponent component;
+
+        public FormRenameComponent(DigitalComponent component = null) {
+            InitializeComponent();
+
+            this.component = component;
+
+            if (component != null)
+                txtComponentLabel.Text = component.Label;
+        }
+
+        private void btnOkay_Click(object sender, EventArgs e) {
+            if (component != null)
+                component.Label = txtComponentLabel.Text;
+
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            DialogResult = DialogResult.Cancel;
+        }
+
+    }
+
+}
